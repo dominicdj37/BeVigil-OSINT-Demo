60a90a1b2c9d3872f1e58b36dbcb0328ea4c6a98
Daniel <dominicdj37@gmail.com>
2022-03-04 16:08:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bevigilosintdemo/core/AppContext.kt
@@ -0,0 +1,16 @@
+package com.example.bevigilosintdemo.core
+
+import android.app.Application
+
+class AppContext: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //todo initialize components
+    }
+
+
+}
+
+
